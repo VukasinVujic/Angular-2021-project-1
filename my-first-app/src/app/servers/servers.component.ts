2b597f69eed47b37a3b1ecba9a7a3570203787b3
vukasinvujic <vukasinvujic021@gmail.com>
2021-08-28 11:36:11 +0200

    ngIF & else

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'no server was creeated';
-  serverName = 'Testserver';
+  serverName = 'Test server';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -19,6 +20,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus =
       'server was created. Name is ' + this.serverName;
   }
