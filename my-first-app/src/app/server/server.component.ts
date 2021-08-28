cc6cd60f900b0f3ef57b58c510f24e8a85a9537d
vukasinvujic <vukasinvujic021@gmail.com>
2021-08-28 11:54:04 +0200

    ngStyle

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -8,7 +8,15 @@ export class ServerComponent {
   serverId = 10;
   serverStatus = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus() {
     return this.serverId;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
