3346b6dec9ce0b38141e10c9467f6df2482f0f3e
vukasinvujic <vukasinvujic021@gmail.com>
2021-08-28 12:59:16 +0200

    preactice 3 and ngFor Index

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   userName = '';
+  showDetails = false;
+  log = [];
+
+  showIt() {
+    this.showDetails = !this.showDetails;
+  }
+
+  onToggleDetails() {
+    this.showDetails = !this.showDetails;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
 }
